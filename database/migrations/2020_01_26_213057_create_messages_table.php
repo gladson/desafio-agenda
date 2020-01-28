827e8757daaa70fbf44ee03a1088fd2bde038259
gladson <gladsonbrito@gmail.com>
2020-01-27 23:48:01 -0400

    add(migrate): add migrate Mensagem

---
+++ database/migrations/2020_01_26_213057_create_messages_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('contacts_id')->index();
+            $table->foreign('contacts_id')
+                ->references('id')
+                ->on('contacts')
+                ->onDelete('cascade');
+
+            $table->text('description');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
